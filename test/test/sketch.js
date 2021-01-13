b0307fad90cd44aa57dbc74aabae971467637ee3
Gina <75906005+CoraMagics@users.noreply.github.com>
2021-01-13 14:30:40 +0100

    Big update

--- test/test/sketch.js
+++ test/test/sketch.js
@@ -26,6 +26,7 @@ let long = 0; // Longitude von 1. Spielerposition
 let lat2 = 0; // Latitude von 2. Spielerposition
 let long2 = 0; // Longitude von 2. Spielerposition
 var database; // db ref
+let egg = true;
 
 let dist = 0.0 ; // Distanzvariable
 
@@ -92,14 +93,46 @@ let n = 100;
 
 function round(dist, n) {
   dist = (Math.round(dist * n) / n);
-  }
+}
 
+let listimg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-
-
+  background(90);
+  loadImage('black.png', img2 => {
+    image(img2, 0, 0, windowWidth, 100);
+    });
+  loadImage('black.png', img3 => {
+    image(img3, 0, windowHeight/2+370, windowWidth, 100);
+    });
+  loadImage('pet.png', img => {
+  image(img, windowWidth/2.7, 100, 443, 745);
+  });
+  loadImage('list.png', listimg => {
+    image(listimg, windowWidth/3, 0, 120, 120);
+    });
+  buttonlist = new Button(windowWidth/3, 0, 120, 120, listimg);
+  loadImage('sound.png', img => {
+    image(img, windowWidth/1.7, 0, 120, 120);
+    });
+  loadImage('clothes.png', img => {
+    image(img, windowWidth/1.7, windowHeight/2+360, 120, 120);
+    });
+  loadImage('food.png', img => {
+    image(img, windowWidth/2.17, windowHeight/2+360, 120, 120);
+    });
+  loadImage('walk.png', img => {
+    image(img, windowWidth/3, windowHeight/2+360, 120, 120);
+    });
+  /*if (egg == false){
+    textSize(30);
+ fill(255);
+  text("Name your pet!", windowWidth/2 -150, windowHeight/3);
+  <input type="text" size="40" name="User"></input>
+  }*/
+ 
+/*
 //create the button
   distance_button = createButton("start walk");
 
@@ -107,16 +140,44 @@ function setup() {
   distance_button.position(windowWidth/2, windowHeight-windowHeight/4);
 // when button is clicked distance measure function is called
   distance_button.mouseClicked(distance_measure);
-
- 
-
-
+*/
 
 
 
   
 }
 
+class Button {
+  
+  constructor(inX, inY, inImg) {
+    this.x = inX;
+    this.y = inY;
+    this.img = inImg;
+  }
+  
+  display() {
+    stroke(0);
+    
+    // tint the image on mouse hover
+    if (this.over()) {
+      tint(204, 0, 128);
+    } else {
+      noTint();
+    }
+    
+    image(this.img, this.x, this.y);
+  }
+  
+  // over automatically matches the width & height of the image read from the file
+  // see this.img.width and this.img.height below
+  over() {
+    if (mouseX > this.x && mouseX < this.x + this.img.width && mouseY > this.y && mouseY < this.y + this.img.height) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 function gen_uid() {
   /*
